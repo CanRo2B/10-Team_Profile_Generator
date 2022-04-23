bd3d8642e07801e7bd1ed119017fd565f0b37332
Candace Robbins <canrob0522@gmail.com>
2022-04-23 16:54:15 -0500

    Added manager test

--- test/test-manager.js
+++ test/test-manager.js
@@ -8,6 +8,16 @@ describe("Set office number via constructor argument", () => {
     expect(e.officeNumber).toBe(officeNumber);
 })
 
-// can get office number
+// can get office number 
+describe("Can get an office number from getOffice()", () =>{
+    const officeNumber = 100;
+    const e = new Manager("Rickey", 1, "ricky@dodge.com", officeNumber);
+    expect(e.getOffice()).toBe(officeNumber);
+});
 
 // role returns manager
+describe("Can get Office Manager getRole function", () => {
+    const manager = "Manager";
+    const e = new Manager("Rickey", 1, "ricky@dodge.com", 100);
+    expect(e.getRole()).toBe(manager);
+});
