f6318344bef4e052800ca9af3cc5bb945b091206
Candace Robbins <canrob0522@gmail.com>
2022-04-23 17:10:57 -0500

    added prompt questions

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+
+const promptUser = () => {
+    return inquirer
+ .prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the team member name?",
+    }, {
+        type: "input",
+        name: "id",
+        message: "What is the team member ID?",
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is the team member email address?",
+    }, {
+        type: "list",
+        name: "role",
+        message: "What is the team member role on the Team?",
+        choices: ["Manager", "Engineer", "Employee", "Intern"]
+    }
+ ])}
